1fa9a5bc3a69df660973e2d20ba2c7731ed57a45
François Sin <fwauters13@gmail.com>
2020-07-26 12:01:16 +0200

    first aliens row

--- assets/js/spaceInvaders.js
+++ assets/js/spaceInvaders.js
@@ -1,7 +1,41 @@
+// Classes-----------------------------------------------------------------------------------------------------------------------
+
+class alienOne {
+    constructor(ctx, x = null, y = null) {
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+    }
+    drawAlienOne(ctx, x, y) {
+        ctx.beginPath();
+        ctx.rect(x+10, y, 5, 5);
+        ctx.rect(x+40, y, 5, 5);
+        ctx.rect(x+15, y+5, 5, 5);
+        ctx.rect(x+35, y+5, 5, 5);
+        ctx.rect(x+10, y+10, 35, 5);
+        ctx.rect(x+5, y+15, 10, 5);
+        ctx.rect(x+20, y+15, 15, 5);
+        ctx.rect(x+40, y+15, 10, 5);
+        ctx.rect(x, y+20, 55, 5);
+        ctx.rect(x, y+25, 5, 10);
+        ctx.rect(x+10, y+25, 35, 5);
+        ctx.rect(x+50, y+25, 5, 10);
+        ctx.rect(x+10, y+30, 5, 5);
+        ctx.rect(x+15, y+35, 10, 5);
+        ctx.rect(x+30, y+35, 10, 5);
+        ctx.rect(x+40, y+30, 5, 5);
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
 // Variables---------------------------------------------------------------------------------------------------------------------
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+
+let aliens = [];
 
 let shipHeight = 10;
 let shipWidth = 40;
@@ -101,6 +135,18 @@ function drawLaser() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    /*let alien = new alienOne();
+    alien.drawAlienOne(ctx, 10, 10);
+
+    let alien2 = new alienOne();
+    alien2.drawAlienOne(ctx, 75, 10);*/
+
+    
+    for (let i = 0; i <= 6; i++) {
+        aliens[i] = new alienOne();
+        aliens[i].drawAlienOne(ctx, 10 + (i*65), 10);
+    }
 
     drawShip();
     drawLaser();
